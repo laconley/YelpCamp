d427870cef6c88bb5717a3c703d9f8a668696478
Lauren Conley <laurenconley789@gmail.com>
2019-04-02 17:39:42 +0000

    remove console log and old mongoose connect code

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
     
-console.log(process.env.DATABASEURL);
+
 mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
-// mongoose.connect("mongodb+srv://lauren_c13:16dmsDUbl5MWVuTY@cluster0-qahig.mongodb.net/yelp_camp_v12?retryWrites=true", {useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
