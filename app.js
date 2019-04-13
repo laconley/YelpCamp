b7f84da84544e7aeb3cc669bfe932b221c6ee8fd
Lauren Conley <laurenconley789@gmail.com>
2019-04-13 12:02:49 -0600

    default port

--- app.js
+++ app.js
@@ -50,6 +50,9 @@ app.use(indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+console.log(process.env.PORT);
+console.log(process.env.IP);
+
+app.listen(process.env.PORT || 5000, process.env.IP, function(){
     console.log("The YelpCamp Server has started");
 });
